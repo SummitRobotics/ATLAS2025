70a73f318b5d36a0a0dfcacbdda553ae95200b84
Cole <>
2025-10-21 17:25:50 -0700

    Response Curves

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,11 +9,10 @@ import static edu.wpi.first.units.Units.*;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
-import edu.wpi.first.math.geometry.Pose2d;
+
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
@@ -21,16 +20,27 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.commands.DriveForwardCommand;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+import frc.robot.util.JoystickResponseCurve;
 import frc.robot.subsystems.AlgaeArm;
 
 public class RobotContainer {
     private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+   
+    private double translationDeadband = 0.05; // 5% deadband
+    private double rotationDeadband = 0.05; // 5% deadband
 
-    /* Setting up bindings for necessary control of the swerve drive platform */
+    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+ private JoystickResponseCurve driveCurve =
+        new JoystickResponseCurve(JoystickResponseCurve.CurveType.LINEAR, translationDeadband);
+    private JoystickResponseCurve rotationCurve =
+       new JoystickResponseCurve(JoystickResponseCurve.CurveType.MILD, rotationDeadband);
+    
+       /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+
             .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+
     private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
     private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
@@ -76,11 +86,19 @@ public class RobotContainer {
         // and Y is defined as to the left according to WPILib convention.
         drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
-            drivetrain.applyRequest(() ->
-                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-                    .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                    .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
-            )
+            drivetrain.applyRequest(() -> {
+                    double leftY = -joystick.getLeftY(); // Drive forward with negative Y (Joystick UP)
+                    double leftX = -joystick.getLeftX(); // Drive left with negative X (Joystick LEFT)
+                    double rightX = -joystick.getRightX(); // Drive counterclockwise with negative X (Joystick LEFT)
+
+                    leftY = driveCurve.apply(leftY);
+                    leftX = driveCurve.apply(leftX);
+                    rightX = rotationCurve.apply(rightX);
+
+                   return drive.withVelocityX(leftY * MaxSpeed) // Drive forward with negative Y (forward)
+                    .withVelocityY(leftX * MaxSpeed) // Drive left with negative X (left)
+                    .withRotationalRate(rightX * MaxAngularRate); // Drive counterclockwise with negative X (left)
+                })
         );
 
         // Idle while the robot is disabled. This ensures the configured
